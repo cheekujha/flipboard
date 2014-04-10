693103ff4d468e69e3fd3c769cb5626073107ac2
Prashant Jha <prashant.jha@studypadinc.com>
2014-04-10 22:34:48 +0530

    touch

--- flipboard_instance.js
+++ flipboard_instance.js
@@ -260,10 +260,13 @@
 		this.dropDown = false;
 		var that = this;
 		if(direction == 'upper' && that.hasNextNews()){
+			alert("111");
 			// console.log("*****************",$(this.currentUpperSelector).css("-webkit-transform"));
 			var time = FlipBoard.getTime($(this.currentUpperSelector));
 			$(this.currentUpperSelector).prefixedCSS('transition','all '+time+'s linear');
+			alert("112");
 			$(this.currentUpperSelector).one($.domPrefixed("transitionend"),function(){
+				alert("113");
 				$(that.currentUpperSelector).prefixedCSS('transition','none');
 				$(that.currentUpperSelector).prefixedCSS('transform','perspective(2000px) rotateX(-'+that.initialIncilination+'deg)').css({'z-index':'1'});
 				//debugger
@@ -275,6 +278,7 @@
 				$(that.nextLowerSelector).html($(that.currentLowerSelector).html());
 				$(that.lowerUpperSelector).html($(that.currentUpperSelector).html());				
 				setTimeout(function(){
+					alert("114");
 					$(that.currentLowerSelector).html($(that.upperLowerSelector).html());
 					$(that.upperLowerSelector).prefixedCSS('transition','none').css({'z-index':'0'});
 					$(that.upperLowerSelector).prefixedCSS('transform','perspective(2000px) rotateX(90deg)');
